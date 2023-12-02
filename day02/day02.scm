bbd91cb8d0b736edb5a4ba39e1838337830ebe79
fjebaker <fergusbkr@gmail.com>
2023-12-02 13:32:13 +0000

    chore: cleanup day02
    Removed some of the debugging code and tidied everything up.

--- day02/day02.scm
+++ day02/day02.scm
@@ -12,23 +12,8 @@
   (green 0)
   (red 0))
 
-(define (display/cubes r)
-  (display "Blue: ")
-  (display (game/cubes-blue r))
-  (display " ; ")
-  (display "Red: ")
-  (display (game/cubes-red r))
-  (display " ; ")
-  (display "Green: ")
-  (display (game/cubes-green r))
-  (display " ; "))
-
 (defstruct game
   id rounds)
-
-(define (display/game g)
-  (display "Game ") (display (game-id g)) (newline)
-  (for-each (lambda (r) (display/cubes r) (newline)) (game-rounds g)))
 
 (define (parse/game-id line)
   (let [(prefix (string-split line ":"))]
@@ -55,9 +40,6 @@
   (let [(id (parse/game-id line)) (rounds (parse/game-rounds line))]
     (make-game id: id rounds: rounds)))
 
-(display/game (parse/game "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"))
-
-
 (define (max/cubes game)
   (let loop [(rounds (game-rounds game)) (blue 0) (red 0) (green 0)]
     (if (null? rounds)
@@ -82,37 +64,24 @@
         (loop (cdr p) (cdr i) (cons (car i) acc))
         (loop (cdr p) (cdr i) acc)))))
 
-(define (part1 lines)
-  (let* [(games (map parse/game lines))
-         (maximums (map max/cubes games))
-         (possible (map is-possible maximums))]
-    (filter/predicated possible games)))
-
 (define (calc-power cube)
   (* (game/cubes-red cube) (game/cubes-blue cube) (game/cubes-green cube)))
-
-(define (part2 lines)
-  (let* [(games (map parse/game lines))
-         (maximums (map max/cubes games))]
-    (apply + (map calc-power maximums))))
 
 (define (getlines filename)
   (call-with-input-file filename (lambda (port) (read-lines port))))
 
-(let [(input (getlines "day02/test.txt"))]
-  (let adder [(games (part1 input)) (sum 0)]
-    (if (null? games)
-      sum
-      (adder (cdr games) (+ sum (game-id (car games)))))))
+(define (solver filename)
+  (let* [(lines (getlines filename))
+         (games (map parse/game lines))
+         (maximums (map max/cubes games))
+         (possible (map is-possible maximums))]
+    (display "Part1: ") 
+    (display (apply + (map (lambda (g) (game-id g)) (filter/predicated possible games))))
+    (newline)
+    (display "Part2: ") 
+    (display (apply + (map calc-power maximums)))
+    (newline)))
 
-(let [(input (getlines "day02/input.txt"))]
-  (let adder [(games (part1 input)) (sum 0)]
-    (if (null? games)
-      sum
-        (adder (cdr games) (+ sum (game-id (car games)))))))
+(solver "day02/test.txt")
 
-(let [(input (getlines "day02/test.txt"))]
-  (part2 input))
-
-(let [(input (getlines "day02/input.txt"))]
-  (part2 input))
+(solver "day02/input.txt")
